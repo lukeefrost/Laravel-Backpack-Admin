1b9c0792b1c901d145c2781a4d2f868b86e2e101
lukelliotfrost <luke@radio.co>
2020-06-07 15:15:08 +0100

    Created CRUD on backpack for tags and customers

--- resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
+++ resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
@@ -2,3 +2,4 @@
 <li class="nav-item"><a class="nav-link" href="{{ backpack_url('dashboard') }}"><i class="la la-home nav-icon"></i> {{ trans('backpack::base.dashboard') }}</a></li>
 <li><a href="{{ backpack_url('log') }}"><i class="fa fa-terminal"></i> <span>Logs</span></a></li>
 <li><a href="{{backpack_url('page') }}"><i class="fa fa-file-o"></i> <span>Pages</span></a></li>
+<li><a href="{{backpack_url('customer') }}"><i class="fa fa-file-o"></i> <span>Customers</span></a></li>
--- routes/backpack/custom.php
+++ routes/backpack/custom.php
@@ -14,4 +14,6 @@ Route::group([
     ],
     'namespace'  => 'App\Http\Controllers\Admin',
 ], function () { // custom admin routes
+    Route::crud('tag', 'TagCrudController');
+    Route::crud('customer', 'CustomerCrudController');
 }); // this should be the absolute last line of this file
